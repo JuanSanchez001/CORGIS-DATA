a6ac1c4092f239eed4a911280388a20dbb3f2085
Juan Sanchez <sanchezjuan4159@gmail.com>
2025-10-22 13:43:44 -0700

    Feat

--- webapp.py
+++ webapp.py
@@ -1,4 +1,7 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, request, render_template, flash
+from markupsafe import Markup
+
+import os
 import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
@@ -9,17 +12,71 @@ def render_main():
 
 @app.route("/p1")
 def render_page1():
-    return render_template('page1.html')
-
+    states = get_state_options()
+    return render_template('page1.html', state_options=states)#, county_options=counties')
+    
 @app.route("/p2")
 def render_page2():
     return render_template('page2.html')
     
-with open('electricity.json', encoding='utf-8') as electricity:
-    electricity_data = json.load(electricity) 
+@app.route("/p3")
+def render_page3():
+    return render_template('page3.html')
+    
+@app.route('/sales')
+def render_sales():
+    states = get_state_options()
+    state = request.args.get('state')
+    name = request.args.get('name')
+   # names = get_name_options()
+    #county = request.args.get('county')
+    name = name_total_sales(state)
+  #  county1 = county_most_bachelors_or_higher(state)
+    sale = "In " + state + ", the county with the highest percentage of under 18 year olds is " + name + "."
+    #sale = name + "in " + state + "has" + sales + "."
+   # sale1 = "In " + state + ", the county with the highest percentage of people with a bachelor's degree or higher is " + name1 + "."
+    
+    return render_template('page1.html', state_options=states, sales=sale)# county_options=counties, 
+    
+def get_state_options():
+    """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
+    with open('electricity.json') as electricity_data:
+        names = json.load(electricity_data)
+    states=[]
+    for n in names:
+        if n["Utility"]["State"] not in states:
+            states.append(n["Utility"]["State"])
+    states.sort()#added ai to sort
+    options=""
+    for s in states:
+        options += Markup("<option value=\"" + s + "\">" + s + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
+    return options    
+    
+def name_total_sales(state):
+    """Return the name of a county in the given state with the highest percent of under 18 year olds."""
+    with open('electricity.json') as electricity_data:
+        names = json.load(electricity_data)
+    highest=0
+    name = ""
+    for t in names:
+        if t["Utility"]["State"] == state:
+           if t["Retail"],["Total"],["Sales"] > highest:
+                highest = ["Retail"],["Total"],["Sales"]
+                name = t["Utility"]["Name"]
+    return name
+    
+#with open('electricity.json', encoding='utf-8') as electricity:
+    #electricity_data = json.load(electricity) 
 
 #print(electricity_data) #word_data is a list of dictionaries
-    
+def is_localhost():
+    """ Determines if app is running on localhost or not
+    Adapted from: https://stackoverflow.com/questions/17077863/how-to-see-if-a-flask-app-is-being-run-on-localhost
+    """
+    root_url = request.url_root
+    developer_url = 'http://127.0.0.1:5000/'
+    return root_url == developer_url
+
 if __name__=="__main__":
     app.run(debug=True)
     
