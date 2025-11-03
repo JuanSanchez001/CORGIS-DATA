b0290b26bc0d4fe130ea3a1dd0484cf8bfa1c392
Juan Sanchez <sanchezjuan4159@gmail.com>
2025-11-03 14:18:08 -0800

    started adding def and states for graph, (not finish)

--- webapp.py
+++ webapp.py
@@ -54,7 +54,7 @@ def render_customers():
     customer3 = f"Total revenue received was ({total_revenue})"
     
     return render_template('page1.html', customers=customer, customers1=customer1, customers2=customer2, customers3=customer3)
-    #sales=sale)# county_options=counties, 
+
     
 @app.route('/sources')   
 def render_sources():
@@ -87,6 +87,78 @@ def get_name_options(state):
         options += Markup("<option value=\"" + n + "\">" + n + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
     return options
     
+    #connect to def demand_average_summer_and_winter() idk?
+    
+   # dataPoints: [{ winter }]
+			#{ y: 212, label: "Italy" },
+			#{ y: 186, label: "China" },
+			#{ y: 272, label: "France" },
+			#{ y: 299, label: "Great Britain" },
+			#{ y: 270, label: "Germany" },
+			#{ y: 165, label: "Russia" },
+			#{ y: 896, label: "CA" } 
+		#]
+       '''  { y: 212, label: "Alabama" }, (states)
+  { y: 186, label: "Alaska" },
+  { y: 272, label: "Arizona" },
+  { y: 299, label: "Arkansas" },
+  { y: 270, label: "California" },
+  { y: 165, label: "Colorado" },
+  { y: 896, label: "Connecticut" },
+  { y: 212, label: "Delaware" },
+  { y: 186, label: "Florida" },
+  { y: 272, label: "Georgia" },
+  { y: 299, label: "Hawaii" },
+  { y: 270, label: "Idaho" },
+  { y: 165, label: "Illinois" },
+  { y: 896, label: "Indiana" },
+  { y: 212, label: "Iowa" },
+  { y: 186, label: "Kansas" },
+  { y: 272, label: "Kentucky" },
+  { y: 299, label: "Louisiana" },
+  { y: 270, label: "Maine" },
+  { y: 165, label: "Maryland" },
+  { y: 896, label: "Massachusetts" },
+  { y: 212, label: "Michigan" },
+  { y: 186, label: "Minnesota" },
+  { y: 272, label: "Mississippi" },
+  { y: 299, label: "Missouri" },
+  { y: 270, label: "Montana" },
+  { y: 165, label: "Nebraska" },
+  { y: 896, label: "Nevada" },
+  { y: 212, label: "New Hampshire" },
+  { y: 186, label: "New Jersey" },
+  { y: 272, label: "New Mexico" },
+  { y: 299, label: "New York" },
+  { y: 270, label: "North Carolina" },
+  { y: 165, label: "North Dakota" },
+  { y: 896, label: "Ohio" },
+  { y: 212, label: "Oklahoma" },
+  { y: 186, label: "Oregon" },
+  { y: 272, label: "Pennsylvania" },
+  { y: 299, label: "Rhode Island" },
+  { y: 270, label: "South Carolina" },
+  { y: 165, label: "South Dakota" },
+  { y: 896, label: "Tennessee" },
+  { y: 212, label: "Texas" },
+  { y: 186, label: "Utah" },
+  { y: 272, label: "Vermont" },
+  { y: 299, label: "Virginia" },
+  { y: 270, label: "Washington" },
+  { y: 165, label: "West Virginia" },
+  { y: 896, label: "Wisconsin" },
+  { y: 212, label: "Wyoming" }'''
+        
+        #dataPoints: [
+			#{ y: 236, label: "Italy" },
+			#{ y: 172, label: "China" },
+			#{ y: 309, label: "France" },
+			#{ y: 302, label: "Great Britain" },
+			#{ y: 285, label: "Ge" },
+			#{ y: 188, label: "Russia" },
+			#{ y: 788, label: "CA" }
+		#]
+        
     
 def total_customers_per_utility(name):
     with open('electricity.json') as electricity_data:
@@ -121,7 +193,15 @@ def highest_electricity_producing_utility(state):
     			name = u["Utility"]["Name"]
     return (name, highest)
     	
-		           
+def demand_average_summer_and_winter()
+    with open('electricity.json') as electricity_data:
+        utilities = json.load(electricity_data)                
+                  
+                  
+                  
+                  
+                  
+                  
                   
 #def name_retail_sale(state):
  #   """Return the name of a county in the given state with the highest percent of sales."""
